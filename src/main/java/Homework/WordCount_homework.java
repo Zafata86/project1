a4750c9f9b63eff0fb17e25eefe40a39ad2f1779
MIHAIL <zafata@abv.bg>
2022-12-12 18:14:17 +0200

    java

---
+++ src/main/java/Homework/WordCount_homework.java
@@ -0,0 +1,28 @@
+// Class name.
+package Homework;
+
+// Import library Scanner.
+
+import java.util.Scanner;
+
+public class WordCount_homework {
+    public static void main(String[] args) {
+
+        // Create a new Scanner name "scanner" and read from console.
+        Scanner scanner = new Scanner(System.in);
+
+        // Read string variable (firstReadString) from console.
+        System.out.print("Please enter a some text : ");
+        String firstReadString = scanner.nextLine();
+        int counterWord = 1;
+        for (int i = 0; i < firstReadString.length(); i++) {
+
+            if(firstReadString.charAt(i) == ' '){
+                counterWord++;
+
+            }
+//
+        }
+        System.out.println(counterWord);
+    }
+}
